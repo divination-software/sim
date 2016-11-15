11c04c2cdf25c3d154e33744132cac58cf6ae22e
cashweaver <cashbweaver@gmail.com>
2016-11-14 18:55:15 -0800

    Add additional server tests

--- server.py
+++ server.py
@@ -1,18 +1,26 @@
 import sys
+import inspect
 from flask import Flask, request
 from simulator import Simulation
+from xml.etree import ElementTree as ET
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def hello_world():
+    if request.content_type != 'application/json':
+        return 'Request must have Content-Type of application/json', 400
+
     json_body = request.get_json()
+    if 'simulation' not in json_body:
+        return 'Request body must contain the key "simulation"', 400
 
-    print(json_body)
+    try:
+        xml = ET.fromstring(json_body['simulation'])
+    except:
+        return 'Value associated with "simulation" must be valid XML', 400
 
-    # incoming post request should be json
-    # json_body['simulation'] should (1) exist and (2) be valid XML
-    Simulation(json_body['simulation'])
+    # Simulation(json_body['simulation'])
 
     return 'Some details!'
 
--- server_tests.py
+++ server_tests.py
@@ -7,9 +7,11 @@ import json
 
 class ServerTestCase(unittest.TestCase):
     def setUp(self):
+        """Run before every test."""
         self.app = server.app.test_client()
 
     def tearDown(self):
+        """Run after every test."""
         pass
 
     def test_a_thing(self):
@@ -18,18 +20,43 @@ class ServerTestCase(unittest.TestCase):
         pass
 
     def test_root_should_error_without_proper_body(self):
-        try:
-            response = self.app.post('/')
-        except TypeError:
-            assert True is True
+        """Should respond with 400 if the POST has no body."""
+        response = self.app.post('/')
+        assert response.status_code == 400
 
-    def test_root_should_return_200(self):
-        data = json.dumps({'simulation': 'Some simulation'})
+    def test_non_json_request_body(self):
+        """Should respond with 400 if provided with non-json Content-Type."""
+        response = self.app.post('/', content_type='plain/text')
+        assert response.status_code == 400
+
+    def test_non_invalid_key_in_json_body(self):
+        """Should respond with 400 if provided with invalid POST data."""
+        bad_data = json.dumps({'someotherkey': 'Some other key value'})
         response = self.app.post('/',
-                                 data=data,
+                                 data=bad_data,
                                  content_type='application/json')
+        assert response.status_code == 400
 
-        assert response.status_code is 200
+    def test_non_invalid_simulation(self):
+        """Should respond with 400 if provided with invalid XML."""
+        bad_data = json.dumps({'simulation': 'Not valid XML'})
+        response = self.app.post('/',
+                                 data=bad_data,
+                                 content_type='application/json')
+        assert response.status_code == 400
+
+    # Should respond with 400 if the XML isn't a valid simulation configuration
+    # Should respond with 200 if the XML is a valid simulation
+    # Should respond with simulation results
+
+    # def test_good_request(self):
+        # """Should respond with simulation results."""
+        # data = json.dumps({'simulation': 'Some simulation'})
+        # response = self.app.post('/',
+                                 # data=data,
+                                 # content_type='application/json')
+
+        # assert response.status_code is 200
 
 if __name__ == '__main__':
     unittest.main()
