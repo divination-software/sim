9f04eac115cd13e621f33968614fb313a6f337c9
cashweaver <cashbweaver@gmail.com>
2016-12-01 14:03:16 -0800

    (feat) Fix generator bug; Working sims

--- simulator/mixins/proceed.py
+++ simulator/mixins/proceed.py
@@ -5,11 +5,9 @@ class Proceed(object):
     """Mixin which provides `proceed`."""
     def proceed(self, edge, entity):
         """Move the simulation along to the next node."""
-        #print('%s is continuing along edge %s' % \
-              #(entity.get_name(), edge))
         target = get_edge(edge)['target']
         node = get_node(target)
-        if type(node).__name__ == 'Exit':
+        if type(node).__name__ in ['Exit', 'Decision']:
             node.run(entity)
         else:
             self.env.process(node.run(entity))
